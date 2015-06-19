53ba7259ffb3de4d0820d2d3aa6c6f64e5c0a4cc
SugarLight256 <nippen12@gmail.com>
2015-06-20 01:30:56 +0900

    HPCircleの表示座標や大きさがずれているバグの修正

--- Assets/scripte/base/HP_Print.cs
+++ Assets/scripte/base/HP_Print.cs
@@ -4,8 +4,6 @@ using System.Collections;
 
 public class HP_Print : MonoBehaviour {
 
-    public GameObject mainCamera;
-    public GameObject baseColor;
     public GameObject Base;
 	// Use this for initialization
 	void Start () {
@@ -14,10 +12,6 @@ public class HP_Print : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 screenPos = mainCamera.GetComponent<Camera>().WorldToScreenPoint(baseColor.transform.position);
-        transform.localPosition = new Vector2(screenPos.x - Screen.width / 2, screenPos.y - Screen.height / 2);
-        transform.localScale = new Vector2(1000,1000) * (1/mainCamera.GetComponent<Camera>().orthographicSize);
-
         transform.GetComponent<Image>().fillAmount = Base.GetComponent<Base_Main>().HPper;
     }
 }
